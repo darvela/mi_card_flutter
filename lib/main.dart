3f2c11a54b4f2939cec84fe9a253d525bebf257d
darvela <darvela@gmail.com>
2020-08-20 21:51:33 -0500

    Challenge 1: containers and columns

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 55.0,
+              ),
+              Container(
+                child: Column(children: [
+                  Container(
+                    height: 225.0,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.green,
+                  ),Container(
+                    height: 225.0,
+                  ),
+                ],
+                )
+              ),
+              Container(
+                width: 55.0,
+              ),
+              Container(
+                width: 100.0,
+                color: Colors.blue,
+              ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
